387f2f14f2e508b9c42d96ecd55c90a42fb2613c
Akash Koodali <akashkoodali9@gmail.com>
2022-10-23 02:22:32 +0530

    Created a next app and added Header and Banner

---
+++ components/Banner.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+
+const Banner = () => {
+  return (
+    <div>
+        <div>
+            {/* <Image src={}></Image> */}
+        </div>
+    </div>
+  )
+}
+
+export default Banner
---
+++ components/Header.tsx
@@ -0,0 +1,60 @@
+import second from "react-icons";
+import { BellIcon, SearchIcon } from "@heroicons/react/solid";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+const Header = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.screenY > 0) {
+        setIsScrolled(true);
+      }else{
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.addEventListener("scroll", handleScroll);
+    }
+  }, []);
+  return (
+    <header className={`${isScrolled && 'bg-[#141414]'}`}>
+      <div className="flex items-center space-x-2 md:space-x-10">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1280px-Netflix_2015_logo.svg.png"
+          alt="logo"
+          height={100}
+          width={100}
+        />
+
+        <ul className="hidden space-x-4 md:flex">
+          <li className="headerLink">Home</li>
+          <li className="headerLink">TV Show</li>
+          <li className="headerLink">Movies</li>
+          <li className="headerLink">New & Popular</li>
+          <li className="headerLink">My List</li>
+        </ul>
+      </div>
+
+      {/* right */}
+      <div className="flex items-center space-x-4 text-sm font-light">
+        <SearchIcon className="hidden sm:inline h-6 w-6" />
+        <p className="hidden lg:inline">Kids</p>
+        <BellIcon className="h-6 w-6" />
+        <Link href={"/account"}>
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+            alt="icon"
+            className="cursor-pointer rounded w-6 h-6"
+          />
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
